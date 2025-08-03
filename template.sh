c243ba97883c74341c604d6d12ebe093f745dc34
Shifat Jim <shifatjim246@gmail.com>
2025-08-04 01:19:20 +0600

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+#crating directories for the project
+
+mkdir -p src
+mkdir -p research
+
+#CREATING FILES FOR THE PROJECT
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch .env
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
